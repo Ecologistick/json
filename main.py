b7b9eb6b48dbc270b9bd3aac8c29239da621288d
Ecologistick <vadikzelenskiy@gmail.com>
2020-11-09 20:26:25 +0000

    split into separate functions

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 from pprint import pprint
-def top(file):
+
+def open_file(file):
   with open(file) as f:
     data = json.load(f)
+  return data
+
+def picking(data):
   counters = {}
   for item in data['rss']['channel']['items']:
     for word in item['description'].lower().split():
@@ -12,6 +16,9 @@ def top(file):
         counters[word] = 1
       else:
         counters[word] += 1
+  return counters
+
+def top(counters):
   return sorted(counters.items(), key=lambda i: i[1], reverse=True)[:10]
   
-pprint(top('newsafr.json'))
+pprint(top(picking(open_file('newsafr.json'))))
